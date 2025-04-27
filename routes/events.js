e3f2328bea6180d8cc9146efe95c7d98024331f5
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2025-04-28 00:15:25 +0800

    Update events.js

--- routes/events.js
+++ routes/events.js
@@ -5,42 +5,53 @@ const router = express.Router();
 let events = [];
 let id = 0;
 
+// Retrieve all events
 router.get("/", (req, res) => {
     res.send(events);
 });
 
+// Add a new event
 router.post("/", (req, res) => {
     events.push({ id: ++id, ...req.body });
     res.send("New event added successfully.");
 });
 
+// Retrieve a specific event by ID
 router.get("/:id", (req, res) => {
     const { id } = req.params;
     const event = events.find((event) => event.id === +id);
 
+    if (!event) {
+        return res.status(404).send({ message: "Event not found" });
+    }
+
     res.send(event);
 });
 
+// Update an event by ID
 router.put("/:id", (req, res) => {
     const { id } = req.params;
     const { titleValue, dateValue, organizerValue } = req.body;
 
-    events.forEach(event => {
-        if (+event.id === +id) {
-            event.title = titleValue;
-            event.date = dateValue;
-            event.organizer = organizerValue;
-        }
-    });
+    const event = events.find((event) => event.id === +id);
+
+    if (!event) {
+        return res.status(404).send({ message: "Event not found" });
+    }
+
+    event.title = titleValue;
+    event.date = dateValue;
+    event.organizer = organizerValue;
     
     res.send("Event successfully updated.");
 });
 
+// Delete an event by ID
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
 
     events = events.filter((event) => event.id !== +id);
-    res.send(id);
+    res.send("Event successfully deleted.");
 });
 
 export default router;
