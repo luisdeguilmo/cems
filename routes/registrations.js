80adab14ffedbde57dafce42c72de915788a6615
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2025-04-28 00:16:29 +0800

    Update registrations.js

--- routes/registrations.js
+++ routes/registrations.js
@@ -2,17 +2,17 @@ import express from 'express';
 import { events } from '../routes/events.js';
 
 const router = express.Router();
+let users = []; 
 
 router.get("/:eventId", (req, res) => {
     const { eventId } = req.params;
-    let listOfAttendees;
 
     events.forEach(event => {
         if (event.id === +eventId) {
-            listOfAttendees = event.attendees;
+            users = event.attendees;
         }
     });
-    res.send(listOfAttendees);
+    res.send(users);
 });
 
 export default router;
